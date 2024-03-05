f923032186fb23af3cb70355cfe3c4a632edf8d3
AnushriPhate <anushrisphate@gmail.com>
2024-03-05 20:03:43 +0530

    first commit

---
+++ learn-spring-orm/src/main/java/com/spring/orm/learnspringorm/LearnSpringOrmApplication.java
@@ -0,0 +1,14 @@
+package com.spring.orm.learnspringorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringOrmApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LearnSpringOrmApplication.class, args);
+	}
+
+}
